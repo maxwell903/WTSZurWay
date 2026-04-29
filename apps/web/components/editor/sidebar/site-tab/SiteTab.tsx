43b59d293fc1357347354fc3905d4e5ed79e43ec
maxwell903 <maxwayne903@gmail.com>
2026-04-29 19:28:03 -0400

    feat(editor): wire AiStockImagesSection into SiteTab

--- apps/web/components/editor/sidebar/site-tab/SiteTab.tsx
+++ apps/web/components/editor/sidebar/site-tab/SiteTab.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import { useEditorStore } from "@/lib/editor-state";
+import { AiStockImagesSection } from "./AiStockImagesSection";
 import { CanvasSettings } from "./CanvasSettings";
 import { FontSelector } from "./FontSelector";
 import { PaletteSelector } from "./PaletteSelector";
 
 export function SiteTab() {
+  const siteId = useEditorStore((s) => s.siteId);
   return (
     <div className="flex h-full flex-col gap-6 overflow-y-auto p-3">
       <PaletteSelector />
+      <AiStockImagesSection siteId={siteId} />
       <FontSelector />
       <CanvasSettings />
     </div>
